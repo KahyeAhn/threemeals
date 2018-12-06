46d91aa589c70fb0c67864503a94ae5d73caec45
lhm2352 <lhm2352@gmail.com>
2018-12-06 12:53:43 +0900

    creat adding_2

--- myblog/settings.py
+++ myblog/settings.py
@@ -40,6 +40,13 @@ INSTALLED_APPS = [
     'fridge',
 ]
 
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        # 'rest_framework.permissions.IsAdminUser',
+    ],
+    'PAGE_SIZE': 10
+}
+
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
