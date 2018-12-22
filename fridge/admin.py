55ed387daa57fc69549835e1e72e8967fc2c1aaa
Kahye Ahn <gprk2626@naver.com>
2018-12-22 16:38:19 +0900

    modeling for initial data of menu

--- fridge/admin.py
+++ fridge/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from fridge.models import Ingredient, ShoppingItem
+from fridge.models import Ingredient, ShoppingItem, Menu, Sauce, Recipe
 
 # Register your models here.
 
 admin.site.register(Ingredient)
 admin.site.register(ShoppingItem)
+admin.site.register(Menu)
+admin.site.register(Sauce)
+admin.site.register(Recipe)
 #admin.site.register(FridgeItem)
